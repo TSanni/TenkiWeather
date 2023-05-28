e8dd342939eca7b924520981c00f196935f8043e
TSanni <tomas.sanni@gmail.com>
2023-05-27 20:00:15 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tomas Sanni on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
