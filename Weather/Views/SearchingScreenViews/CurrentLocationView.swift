11f19600b24cad0af38bef18e4d0bf316e2c47a7
Tomas Sanni <tomas.sanni7@gmail.com>
2024-01-25 16:39:57 -0600

    complete search screen ui update

---
+++ Weather/Views/SearchingScreenViews/CurrentLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentLocationView.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 1/25/24.
+//
+
+import SwiftUI
+
+struct CurrentLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentLocationView()
+}
---
+++ Weather/Views/SearchingScreenViews/SavedLocationImageView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedLocationImageView.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 1/25/24.
+//
+
+import SwiftUI
+
+struct SavedLocationImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedLocationImageView()
+}
