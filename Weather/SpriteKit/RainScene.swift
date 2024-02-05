448b03a7ed9c0ad15c20794d09d971b7e54df25e
Tomas Sanni <tomas.sanni7@gmail.com>
2024-02-05 13:20:55 -0600

    introduce spritekit and add snow support

---
+++ Weather/SpriteKit/RainScene.swift
@@ -0,0 +1,8 @@
+//
+//  RainScene.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 2/5/24.
+//
+
+import Foundation
---
+++ Weather/SpriteKit/SnowScene.swift
@@ -0,0 +1,8 @@
+//
+//  SnowScene.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 2/5/24.
+//
+
+import Foundation
