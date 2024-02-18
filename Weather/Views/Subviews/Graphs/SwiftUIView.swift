ced54d7975a9f2ce1319c4d02322acb114c9d2d7
Tomas Sanni <tomas.sanni7@gmail.com>
2024-02-18 16:24:09 -0600

    update WindModel to accept default WeatherKit types

---
+++ Weather/Views/Subviews/Graphs/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 2/18/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
