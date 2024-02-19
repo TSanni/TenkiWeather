bfc09337c4d92253e7b808ad94110e67279f0be8
Tomas Sanni <tomas.sanni7@gmail.com>
2024-02-19 16:08:57 -0600

    change TomorrowWeatherModel to DailyWeatherModel. delete old DailyWeatherModel file

--- Weather/Models/DailyWeatherModel.swift
+++ Weather/Models/DailyWeatherModel.swift
@@ -2,157 +2,148 @@
 //  DailyWeatherModel.swift
 //  Weather
 //
-//  Created by Tomas Sanni on 6/10/23.
+//  Created by Tomas Sanni on 6/9/23.
 //
 
 import Foundation
 import WeatherKit
+import SwiftUI
+import SpriteKit
 
-//MARK: - Main Model for Daily Weather
-/// Daily Weather Model - Includes properties for daily weather to be used with WeatherKit
+//MARK: - Main Model
 struct DailyWeatherModel: Identifiable {
     let id = UUID()
+    let highTemperature: Measurement<UnitTemperature>
+    let lowTemperature: Measurement<UnitTemperature>
+    let precipitation: Precipitation
+    let precipitationChance: Double // value between 0 and 1
+    let snowfallAmount: Measurement<UnitLength>
+    let moonEvents: MoonEvents? //TODO: create a MoonEvents object
+    let sunData: SunData
+    let tomorrowWind: WindData
+    let date: Date
+    let condition: WeatherCondition
+    let uvIndexValue: Int
+    let uvIndexCategory: UVIndex.ExposureCategory
+    let symbolName: String
+    let precipitationAmount: Measurement<UnitLength>
     
-    let date: String
-    let dailyWeatherDescription: WeatherCondition
-    let dailyChanceOfPrecipitation: String // Will be used twice in daily view
-    let dailySymbol: String
-    let dailyLowTemp: String
-    let dailyHighTemp: String
-    
-    let dailyWind: WindData
-    let dailyUVIndex: String
-    let sunEvents: SunData
-    let hourlyTemperatures: [HourlyModel]
+    let tomorrowDetails: DetailsModel
+//    let tomorrowHourlyWind: [WindData] // delete
+    let hourlyWeather: [HourlyModel]
+    let timezone: Int
 
     
+    var readableDate: String {
+        return getDayOfWeekAndDate(date: date, timezoneOffset: timezone)
+    }
     
-    /// Holder data for daily weather
+    var tomorrowHigh: String {
+        let temperature = highTemperature.converted(to: getUnitTemperature())
+        let temperatureValueOnly = convertNumberToZeroFloatingPoints(number: temperature.value)
+        return temperatureValueOnly
+    }
     
-    static let dailyDataHolder: [DailyWeatherModel] = [
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
+    var tomorrowLow: String {
+        let temperature = lowTemperature.converted(to: getUnitTemperature())
+        let temperatureValueOnly = convertNumberToZeroFloatingPoints(number: temperature.value)
+        return temperatureValueOnly
+    }
+    
+    var precipitationType: String {
+        precipitation.description
+    }
+    
+    var tomorrowChanceOfPrecipitation: String {
+        precipitationChance.formatted(.percent)
+    }
+    
+    //snowfallAmount
+    //rainfallAmount
+    //moon events
+    
+    var tomorrowWeatherDescription: String {
+        condition.description
+    }
+    
+    var uvIndexNumber: String {
+        uvIndexValue.description
+    }
+    
+    var uvIndexCategoryType: String {
+        uvIndexCategory.description
+    }
+    
+    //precipitationAmount
         
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-        
-        DailyWeatherModel(
-            date: "-",
-            dailyWeatherDescription: .clear,
-            dailyChanceOfPrecipitation: "-",
-            dailySymbol: "-",
-            dailyLowTemp: "-",
-            dailyHighTemp: "-",
-            dailyWind: WindData.windDataHolder[0],
-            dailyUVIndex: "-",
-            sunEvents: SunData.sunDataHolder, hourlyTemperatures: HourlyModel.hourlyTempHolderData
-        ),
-    ]
+ 
+    var backgroundColor: Color {
+        K.getBackGroundColor(symbol: symbolName)
+    }
+    
+    var scene: SKScene? {
+        print("THE SYMBOL FOR TOMORROW SCENE: \(symbolName)")
+        return  K.getScene(symbol: symbolName)
+    }
+    
 
+    
+    static let placeholder = DailyWeatherModel(
+        highTemperature: Measurement(value: 50, unit: .fahrenheit),
+        lowTemperature: Measurement(value: 50, unit: .fahrenheit),
+        precipitation: Precipitation.hail,
+        precipitationChance: 0.5,
+        snowfallAmount:  Measurement(value: 0.5, unit: .centimeters),
+        moonEvents: nil,
+        sunData: SunData.sunDataHolder,
+        tomorrowWind: WindData.windDataHolder[0],
+        date: Date.distantFuture,
+        condition: .blizzard,
+        uvIndexValue: 5,
+        uvIndexCategory: .extreme,
+        symbolName: "cloud.rain",
+        precipitationAmount: Measurement(value: 0.5, unit: .centimeters),
+        tomorrowDetails: DetailsModel.detailsDataHolder,
+//        tomorrowHourlyWind: WindData.windDataHolder,
+        hourlyWeather: HourlyModel.hourlyTempHolderData,
+        timezone: 0
+    )
 }
 
 
-
-
+extension DailyWeatherModel {
+    
+    /// Takes a Double, removes floating point numbers, then converts to and returns a String
+    private func convertNumberToZeroFloatingPoints(number: Double) -> String {
+        let convertedStringNumber = String(format: "%.0f", number)
+        return convertedStringNumber
+    }
+    
+    /// This functions accepts a date and returns a string of that date in a readable format
+    /// Ex: Tuesday, July 7
+    private func getDayOfWeekAndDate(date: Date, timezoneOffset: Int) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM d"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: timezoneOffset)
+        
+        let readableDate = dateFormatter.string(from: date)
+        return readableDate
+    }
+    
+    /// Checks UserDefaults for UnitTemperature selection. Returns the saved Unit Temperature.
+    private func getUnitTemperature() -> UnitTemperature {
+        let savedUnitTemperature = UserDefaults.standard.string(forKey: K.UserDefaultKeys.unitTemperatureKey)
+        
+        switch savedUnitTemperature {
+        case K.TemperatureUnits.fahrenheit:
+            return .fahrenheit
+        case K.TemperatureUnits.celsius:
+            return .celsius
+        case   K.TemperatureUnits.kelvin:
+            return .kelvin
+        default:
+            print("CAN'T DETERMINE UNIT TEMPERATURE")
+            return .fahrenheit
+        }
+    }
+}
