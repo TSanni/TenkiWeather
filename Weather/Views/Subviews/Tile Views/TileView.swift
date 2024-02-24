b91164c9a6c3b02c6dcd82b2332384658b600fca
Tomas Sanni <tomas.sanni7@gmail.com>
2024-02-24 17:44:51 -0600

    add TileView

---
+++ Weather/Views/Subviews/Tile Views/TileView.swift
@@ -0,0 +1,18 @@
+//
+//  TileView.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 2/23/24.
+//
+
+import SwiftUI
+
+struct TileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TileView()
+}
