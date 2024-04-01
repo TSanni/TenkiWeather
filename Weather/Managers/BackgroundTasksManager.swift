1dd29e0241ce9df81c69e733badb4b50cf9c2dd3
Tomas Sanni <tomas.sanni7@gmail.com>
2024-03-31 19:18:55 -0500

    add background tasks. convert searchedLocationDictionary type to Model

---
+++ Weather/Managers/BackgroundTasksManager.swift
@@ -0,0 +1,8 @@
+//
+//  BackgroundTasksManager.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 3/28/24.
+//
+
+import Foundation
---
+++ Weather/Models/Sub Models/SearchLocationModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchLocationModel.swift
+//  Tenki Weather
+//
+//  Created by Tomas Sanni on 3/31/24.
+//
+
+import Foundation
